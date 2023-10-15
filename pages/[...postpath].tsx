d608ffd287f8e8acfc9051eb0fb5c5a48a0de592
Imam Din <41516850+Imamdin81@users.noreply.github.com>
2023-10-15 17:40:50 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -17,7 +17,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 		return {
 			redirect: {
-				permanent: false,
+				permanent: true,
 				destination: `${
 					`https://comicsconnoisseurs.com/` + encodeURI(path as string)
 				}`,
